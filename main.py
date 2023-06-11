04b2b5b11b1ed702ef6f5eadd22aa1697c639c98
geethan <gnanageethan2000@gmail.com>
2023-06-11 15:53:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def fun(x):
+    print(x)
+b = 5   
+fun(b)
+
+num1 = int(input("enter the number here : "))
+num2 =int(input("enter  the num here : "))
+print("total",num1+num2)
+
+print("total : ",(lambda x,y:x+y)(int(input("enter x ")),int(input("enter y :"))))
+
+if 10<20:
+    print("hello")
+5//3
+
+i = 1 
+while i <= 5:
+    print(i*'*')
+    i+=i
+    
